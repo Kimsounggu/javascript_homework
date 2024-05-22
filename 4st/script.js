bc510255b25a5b7a92a2d86cee4ce6172ef130da
Kimsounggu <tjdrn4657@naver.com>
2024-05-22 19:54:32 +0900

    feat : 5주차 과제 완료

---
+++ 4st/script.js
@@ -0,0 +1,20 @@
+// const user = {
+//   이름: "병수",
+//   나이: 20,
+//   주소: "시골",
+// };
+
+// let users = localStorage.setItem("user", JSON.stringify(user));
+
+// let outUsers = JSON.parse(localStorage.getItem("user"));
+
+// let newUsers = { ...outUsers, 나이: 30 };
+
+// localStorage.setItem("user", JSON.stringify(newUsers));
+
+// localStorage.removeItem("user");
+// console.log(localStorage.getItem("user"));
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((res) => res.json())
+  .then((Data) => console.log(Date));
